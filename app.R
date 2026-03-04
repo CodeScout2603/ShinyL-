f14b774df893040975e700c069e18c39a0923ae4
CodeScout2603 <Bela.Rakosi@TrOn-Mainz.DE>
2026-03-04 15:12:44 +0100

    veränderung

--- app.R
+++ app.R
@@ -4,35 +4,31 @@ library(golubEsets)
 library(RColorBrewer)
 library(pheatmap)
 
-options(shiny.error = browser)   # <--- DEBUG-MODUS: zeigt alle Fehler
-
 # -------------------------
-# Daten laden und vorbereiten
+# Daten laden
 # -------------------------
 data(Golub_Train)
 
 x <- exprs(Golub_Train)
 
+# Samples benennen
 sample_labels <- paste(
   pData(Golub_Train)$Samples,
   pData(Golub_Train)$ALL.AML,
   sep = "_"
 )
-
 colnames(x) <- sample_labels
 
+# Log2-Transformation (robust)
 xLog <- log2(pmax(x, 1))
 
-num_ALL <- sum(pData(Golub_Train)$ALL.AML == "ALL")
-num_AML <- sum(pData(Golub_Train)$ALL.AML == "AML")
-
+# Varianzberechnung einmal
 geneVariance <- apply(xLog, 1, var)
 sortedGenes <- names(sort(geneVariance, decreasing = TRUE))
 
-annotation <- data.frame(
-  Leukemia = pData(Golub_Train)$ALL.AML
-)
-rownames(annotation) <- colnames(xLog)
+# Patientenzahlen
+num_ALL <- sum(pData(Golub_Train)$ALL.AML == "ALL")
+num_AML <- sum(pData(Golub_Train)$ALL.AML == "AML")
 
 # -------------------------
 # UI
@@ -50,28 +46,30 @@ ui <- fluidPage(
       ),
       
       sliderInput("numberOfGenes",
-                  "Number of Genes",
-                  min = 10,
-                  max = 100,
-                  value = 50),
+        "Number of Genes",
+        min = 10,
+        max = 100,
+        value = 50
+      ),
       
       selectInput("distMea",
-                  "Distance Measure",
-                  choices = c("euclidean", "maximum",
-                              "manhattan", "canberra",
-                              "binary", "minkowski")),
+        "Distance Measure",
+        choices = c("euclidean", "maximum",
+                    "manhattan", "canberra",
+                    "binary", "minkowski")
+      ),
       
       selectInput("clustMeth",
-                  "Clustering Method",
-                  choices = c("ward.D", "ward.D2",
-                              "single", "complete",
-                              "average", "mcquitty",
-                              "median", "centroid"))
+        "Clustering Method",
+        choices = c("ward.D", "ward.D2",
+                    "single", "complete",
+                    "average", "mcquitty",
+                    "median", "centroid")
+      )
     ),
     
     mainPanel(
-      plotOutput("heatmap", height = "900px"),
-      verbatimTextOutput("debug")   # <-- DEBUG-FENSTER
+      plotOutput("heatmap", height = "900px")
     )
   )
 )
@@ -85,29 +83,19 @@ server <- function(input, output) {
     xLog[sortedGenes[1:input$numberOfGenes], , drop = FALSE]
   })
   
-  output$debug <- renderPrint({
-    list(
-      selected_genes_dim = dim(selectedGenes()),
-      colnames_match = identical(colnames(selectedGenes()), rownames(annotation)),
-      annotation_rows = rownames(annotation)[1:5],
-      matrix_cols = colnames(selectedGenes())[1:5]
-    )
-  })
-  
   output$heatmap <- renderPlot({
     
     mat <- selectedGenes()
     
+    # pheatmap MUSS mit print() gerendert werden
     print(
       pheatmap(
         mat,
         clustering_distance_rows = input$distMea,
         clustering_distance_cols = input$distMea,
         clustering_method = input$clustMeth,
-        annotation_col = annotation,
         color = colorRampPalette(brewer.pal(8, "Blues"))(25),
-        fontsize = 9,
-        main = "Heatmap der Top-variablen Gene"
+        fontsize = 8
       )
     )
   })
